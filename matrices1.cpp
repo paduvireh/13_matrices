4639d43a6a2ebe85d0a89bb8ae7671deb79326fb
paduvireh <paduvireh@unjbg.edu.pe>
2024-07-16 23:04:24 -0500

    Matrices1: Se agrego suma de elementos arriba de la diagonal principal

--- matrices1.cpp
+++ matrices1.cpp
@@ -13,5 +13,13 @@ int main(){
 			cin>>M[i][j];
 		}
 	}
+	int S=0;
+	for(int i=0; i<n; i++){
+		for(int j=i+1; j<n; j++){
+			S=S+M[i][j];
+		}
+	}
+	cout<<"La suma de los elementos que se encuentran arriba de la diagonal principal es: "<<endl;
+	cout<<S<<endl;
 	return 0;
 }
